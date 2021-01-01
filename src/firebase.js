8b36a81c93b2a579ca0f9ca0882a611fb3dd1e74
Shahrukh223 <57192850+Shahrukh223@users.noreply.github.com>
2021-01-01 17:11:55 -0600

    1/1/21

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCikd7bucNM6GQdEnb18CbgvR5UcHbOi50",
+    authDomain: "instant-messenger-93aa5.firebaseapp.com",
+    projectId: "instant-messenger-93aa5",
+    storageBucket: "instant-messenger-93aa5.appspot.com",
+    messagingSenderId: "253432754107",
+    appId: "1:253432754107:web:5f21b6dc556126bb7f9e9e",
+    measurementId: "G-BP3RK0TK6N"
+  };
