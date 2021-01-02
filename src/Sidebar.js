208e996c4365add034e0b8dcdf194d29b77033b4
Shahrukh223 <57192850+Shahrukh223@users.noreply.github.com>
2021-01-01 18:02:39 -0600

    header
    added side bar search bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import {Avatar, Icon, IconButton} from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {SearchOutlined} from '@material-ui/icons';
 
 function Sidebar() {
     return (
@@ -28,8 +29,10 @@ function Sidebar() {
             </div>
 
             <div className= "sidebar_search">
-
-
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder= "Search or Start a new Chat" type = "text"/>
+                </div>
             </div>
 
             <div className= "sidebar_chats">
